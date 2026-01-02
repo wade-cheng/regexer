04781f7e346f48543e50b6cbce356b5553ef0b64
Wade Cheng <a>
2026-01-02 00:24:43 -0500

    impl mvp
    writing a tui is too much work for something that could be so simple. I'd like to actually use this thing, please. https://xkcd.com/1205/

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,124 @@
-use anyhow::{Context, Result};
-use crossterm::event;
-use crossterm::event::Event;
-use regexer::State;
+use std::{fmt, fs, path::PathBuf, str::FromStr, sync::LazyLock};
 
-use std::sync::mpsc::{self, Sender};
-use std::thread;
+use anyhow::{Context, Error, Result, anyhow};
+use onlyargs_derive::OnlyArgs;
+use regex::Regex;
 
-// use crossterm::style::Stylize as _;
-//
-// for y in 0..40 {
-//     for x in 0..150 {
-//         if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
-//             // in this loop we are more efficient by not flushing the buffer.
-//             stdout
-//                 .queue(cursor::MoveTo(x, y))?
-//                 .queue(style::PrintStyledContent("█".magenta()))?;
-//         }
-//     }
-// }
-//
-// stdout.flush()?;
+// Don't need a Args rustdoc here because our onlyargs scrapes from the Cargo.toml description I guess??
+#[derive(OnlyArgs)]
+struct Args {
+    /// The source file to run regex replacements on.
+    file: PathBuf,
+    /// The file of regex replacements.
+    replacements: PathBuf,
+    /// Output file name, if desired.
+    out: Option<PathBuf>,
+}
 
-fn main() -> Result<()> {
-    ctrlc::set_handler(regexer::exit_with_cleanup).context("Error setting Ctrl-C handler")?;
+struct RegexCommand {
+    find: String,
+    find_compiled: Regex,
+    replace: String,
+}
 
-    let state = regexer::startup()?;
-    let tx = start_logic_thread(state);
-
-    loop {
-        if let Ok(e) = event::read() {
-            tx.send(e)
-                .expect("Could not send an event to the logic thread.");
-        }
+impl fmt::Debug for RegexCommand {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "RegexCommand {{ find: \"{}\", replace: \"{}\" }}",
+            self.find, self.replace
+        )
     }
 }
 
-fn start_logic_thread(initial_state: State) -> Sender<Event> {
-    let (tx, rx) = mpsc::channel();
+/// Regex to parse a RegexCommand from a line of text.
+static COMMAND_FROM_LINE: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r#""(.*)"\s*?->\s*?"(.*)""#).unwrap());
 
-    // thead "joined" by exiting the program
-    thread::spawn(move || {
-        let mut state = initial_state;
-        loop {
-            let e = rx.recv().expect("Channel shouldn't hang.");
-            regexer::handle_event(e);
+impl RegexCommand {
+    fn new(search: String, replace: String) -> Result<Self> {
+        let search_compiled = Regex::new(&search)?;
+        Ok(Self {
+            find: search,
+            find_compiled: search_compiled,
+            replace,
+        })
+    }
+
+    fn from_line(line: &str) -> Result<Self> {
+        assert!(!line.contains("\n") && !line.contains("\r\n"));
+
+        let (_, [search, replace]) = COMMAND_FROM_LINE
+            .captures(line)
+            .context("could not parse line. regex failed to find.")?
+            .extract();
+
+        // program in escape sequences
+        let replace = replace.replace("\\n", "\n");
+        let replace = replace.replace("\\r", "\r");
+        let replace = replace.replace("\\\\", "\\");
+
+        Self::new(search.to_string(), replace)
+    }
+
+    /// Transmorgify subject matter using our stored regex command.
+    fn transmorgify(&self, subject: &str) -> String {
+        self.find_compiled
+            .replace_all(subject, &self.replace)
+            .to_string()
+    }
+}
+
+/// A series of find and replace commands.
+#[derive(Default)]
+struct RegexCommands(Vec<RegexCommand>);
+
+impl RegexCommands {
+    /// Transmorgify subject matter using our series of stored regex commands.
+    fn transmorgify(&self, subject: &String) -> String {
+        let mut subject = subject.to_owned();
+        for command in &self.0 {
+            subject = command.transmorgify(&subject);
         }
-    });
+        subject.replace("\r\n", "\n").replace("\u{00A0}", " ")
+    }
+}
 
-    tx
+impl FromStr for RegexCommands {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut commands = RegexCommands(vec![]);
+
+        for (i, line) in s.replace("\r\n", "\n").split("\n").enumerate() {
+            if line.is_empty() {
+                log::trace!("line {i} IGNORED");
+                continue;
+            }
+
+            let command = RegexCommand::from_line(line)
+                .context(anyhow!("failure on line {i} of replacements file"))?;
+            log::trace!("line {i} parsed a {command:?}");
+            commands.0.push(command);
+        }
+
+        Ok(commands)
+    }
+}
+
+fn main() -> Result<()> {
+    simple_logger::SimpleLogger::new().init().unwrap();
+
+    let config: Args = onlyargs::parse()?;
+
+    let source = fs::read_to_string(&config.file)?;
+    let replacement_commands = RegexCommands::from_str(&fs::read_to_string(config.replacements)?)?;
+    let out_text = replacement_commands.transmorgify(&source);
+    let dest_file: String = config
+        .out
+        .map(|pbuf| pbuf.to_string_lossy().to_string())
+        .unwrap_or(config.file.to_string_lossy().to_string() + ".replaced");
+    fs::write(dest_file, out_text)?;
+
+    Ok(())
 }
